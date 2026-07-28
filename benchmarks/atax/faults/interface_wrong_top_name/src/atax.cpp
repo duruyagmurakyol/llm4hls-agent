eb15d70536b757a4cd0a386a17f95dad272cc517
duruyagmurakyol <duruyagmurakyol@gmail.com>
2026-07-28 18:04:39 +0100

    Add ATAX interface fault source

---
+++ benchmarks/atax/faults/interface_wrong_top_name/src/atax.cpp
@@ -0,0 +1,17 @@
+#include "atax.h"
+
+void kernel_atax_wrong(double A[38][42], double x[42], double y[42], double tmp[38]) {
+#pragma HLS top name = kernel_atax
+    const int m = 38;
+    const int n = 42;
+    int i, j;
+    for (i = 0; i < n; i++)
+        y[i] = 0;
+    for (i = 0; i < m; i++) {
+        tmp[i] = 0.0;
+        for (j = 0; j < n; j++)
+            tmp[i] = tmp[i] + A[i][j] * x[j];
+        for (j = 0; j < n; j++)
+            y[j] = y[j] + A[i][j] * tmp[i];
+    }
+}
