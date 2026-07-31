8622ec67f73dcac03ebbfd335ef5b1ccba027a20
duruyagmurakyol <duruyagmurakyol@gmail.com>
2026-07-31 12:28:24 +0100

    Add parser-compatible vector-add HLS TCL

---
+++ benchmarks/hls_eval/vector_add/scripts/run_agent_baseline.tcl
@@ -0,0 +1,11 @@
+open_project -reset /tmp/llm4hls-agent/vector_add_baseline
+set_top vector_add
+add_files -cflags "-I/home/xilinx/projects/llm4hls-agent/benchmarks/hls_eval/vector_add/src" /home/xilinx/projects/llm4hls-agent/benchmarks/hls_eval/vector_add/src/vector_add.cpp
+add_files /home/xilinx/projects/llm4hls-agent/benchmarks/hls_eval/vector_add/src/vector_add.h
+add_files -tb -cflags "-I/home/xilinx/projects/llm4hls-agent/benchmarks/hls_eval/vector_add/src" /home/xilinx/projects/llm4hls-agent/benchmarks/hls_eval/vector_add/testbench/vector_add_tb.cpp
+open_solution -reset solution1
+set_part xczu3eg-sfvc784-2-e
+create_clock -period 10 -name default
+csim_design
+csynth_design
+exit
