a0b9484acfa7e9eb32e78db7fcc480f9a070d0ec
duruyagmurakyol <duruyagmurakyol@gmail.com>
2026-08-08 00:46:44 +0100

    Restore portable ATAX candidate TCL

---
+++ benchmarks/hls_eval/atax/scripts/run_candidate_3b.tcl
@@ -0,0 +1,16 @@
+open_project -reset atax_candidate_3b_project
+set_top kernel_atax
+
+add_files ../candidates/atax_candidate_3b.cpp
+add_files ../candidates/atax.h
+add_files -tb -cflags "-I../src" ../testbench/atax_tb.cpp
+
+open_solution -reset solution1
+
+set_part {xczu3eg-sfvc784-2-e}
+create_clock -period 10 -name default
+
+csim_design
+csynth_design
+
+exit
