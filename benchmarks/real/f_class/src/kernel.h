1fd86fe1ef0457ee93203b48e6a1a73c9d32f999
Duru Yagmur Akyol <duruyagmurakyol@gmail.com>
2026-08-08 00:01:55 +0100

    Delete real benchmark

--- benchmarks/real/f_class/src/kernel.h
+++
@@ -1,5 +0,0 @@
-#ifndef KERNEL_H
-#define KERNEL_H
-#define N 64
-void vadd(const int a[N], const int b[N], int out[N]);
-#endif
--- benchmarks/real/f_class/work/hls/csim/build/obj/kernel.d
+++
@@ -1,2 +0,0 @@
-obj/kernel.o: ../../../../src/kernel.cpp \
-  /home/xilinx/projects/llm4hls-agent/benchmarks/real/f_class/src/kernel.h
