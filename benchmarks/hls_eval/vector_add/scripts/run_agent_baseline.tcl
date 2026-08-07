6d02e0341fdbc46206c4f06a508266e4db7afe0a
duruyagmurakyol <duruyagmurakyol@gmail.com>
2026-08-07 01:34:40 +0100

    Align vector-add HLS baseline with U55C

--- benchmarks/hls_eval/vector_add/scripts/run_agent_baseline.tcl
+++ benchmarks/hls_eval/vector_add/scripts/run_agent_baseline.tcl
@@ -4,7 +4,7 @@ add_files -cflags "-I/home/xilinx/projects/llm4hls-agent/benchmarks/hls_eval/vec
 add_files /home/xilinx/projects/llm4hls-agent/benchmarks/hls_eval/vector_add/src/vector_add.h
 add_files -tb -cflags "-I/home/xilinx/projects/llm4hls-agent/benchmarks/hls_eval/vector_add/src" /home/xilinx/projects/llm4hls-agent/benchmarks/hls_eval/vector_add/testbench/vector_add_tb.cpp
 open_solution -reset solution1
-set_part xczu3eg-sfvc784-2-e
+set_part xcu55c-fsvh2892-2L-e
 create_clock -period 10 -name default
 csim_design
 csynth_design
