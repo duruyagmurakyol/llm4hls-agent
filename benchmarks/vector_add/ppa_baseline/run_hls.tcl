6a60620e53880d39e3395d1ca6fddec5f58c2e12
duruyagmurakyol <duruyagmurakyol@gmail.com>
2026-08-08 01:26:46 +0100

    Target vector-add PPA TCL at U55C

--- benchmarks/vector_add/ppa_baseline/run_hls.tcl
+++ benchmarks/vector_add/ppa_baseline/run_hls.tcl
@@ -2,7 +2,7 @@ open_project -reset vector_add_hls
 set_top vector_add
 add_files src/vector_add.cpp -cflags "-Isrc"
 open_solution -reset solution1 -flow_target vivado
-set_part {xczu3eg-sfvc784-2-e}
+set_part {xcu55c-fsvh2892-2L-e}
 create_clock -period 10 -name default
 csynth_design
 exit
